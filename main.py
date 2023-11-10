5040f9bc8d35e980f1b0baeaf2ccc1f5497b16eb
JeserRamirez <jeserramirez2002@outlook.com>
2023-11-09 23:18:23 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from ply import lex
 
 app = Flask(__name__)
 
@@ -14,16 +15,58 @@ def checkSintaxisCorrect(entrada):
         return True
     return "Error de sintaxis"
 
+tokens = (
+    'NUMERO',
+    'SUMA',
+    'RESTA',
+    'MULTIPLICACION'
+)
+
+t_SUMA = r'\+'
+t_RESTA = r'\-'
+t_MULTIPLICACION = r'\*'
+
+def t_NUMERO(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+t_ignore = ' \t'
+
+def t_error(t):
+    print(f'Caracter erroneo: {t.value[0]}')
+    t.lexer.skip(1)
+
+analizadorLexico = lex.lex()
+
+def analizarLexico():
+    codigoFuente = "2 + 1"
+    analizadorLexico.input(codigoFuente)
+
+    for token in analizadorLexico:
+        print(token)
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     cadenaProcesada = None
+
+    analizarLexico()
 
     if request.method == "POST":
         entrada = request.form.get("entrada")
         cadenaProcesada = procesarCadena(entrada)
 
     return render_template("index.html", title="Lenguajes y Automatas II", cadenaProcesada=cadenaProcesada)
+
+@app.route("/analisis-sintactico.html")
+def analisis_sintactico():
+    # Lógica para mostrar la página de análisis sintáctico
+    return render_template("analisis-sintactico.html")
+
+@app.route("/analisis-lexico.html")
+def analisis_lexico():
+    # Lógica para mostrar la página de análisis sintáctico
+    return render_template("analisis-lexico.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
